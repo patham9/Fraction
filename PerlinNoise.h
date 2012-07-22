41a289681f7f549d44aee556575dbf912d378754
Patrick Hammer <patham9@gmail.com>
2012-07-22 03:30:13 +0200

    Initial import

---
+++ PerlinNoise.h
@@ -0,0 +1,8 @@
+//perlin noise directly converted C# code from //http://devmag.org.za/2009/04/25/perlin-noise/
+#define NULL 0
+#include "Hutil.h" 
+float **fArray2D(int width,int height);
+float Interpolate(float x0, float x1, float alpha);
+float **WhiteNoise(int width, int height);
+float** GenerateSmoothNoise(int baseNoiseLen, int baseNoiseLen0,float** baseNoise, int octave);
+float** GeneratePerlinNoise(int baseNoiseLen, int baseNoiseLen0,float** baseNoise, int octaveCount);
